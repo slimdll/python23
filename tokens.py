0faad5acd7f20ff4abf22485f4ad3031e6b2bbec
slimdll <37386769+slimdll@users.noreply.github.com>
2018-03-15 15:15:41 +0200

    Add files via upload

---
+++ tokens.py
@@ -0,0 +1,31 @@
+__author__ = 'Marcos'
+
+# Telegram Python Bot Token API
+telegram_bot_python = '********:***********************'
+
+# Telegram SMS Bot Token API
+telegram_bot_sms = '********:***********************'
+
+# Telegram Instagram Bot Token API
+telegram_bot_instagram = '********:***********************'
+
+# Telegram Wallpapers Bot Token API
+telegram_bot_wallpapers = '********:***********************'
+
+# Twitter Tokens
+consumer_key = "********:***********************"
+consumer_secret = "********:***********************"
+access_key = "********-********:***********************"
+access_secret = "********************************"
+
+# Sinch SMS
+key_sinch = '*********-****-****-****-************'
+secret_sinch = '******************=='
+
+# Facebook Graph API
+graph_api_token = '**********************************************************************'
+
+# Instagram API
+client_id = '********************************'
+client_secret = '********************************'
+access_token = '***********.******.********************************'
